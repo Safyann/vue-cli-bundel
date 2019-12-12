4ae1dde14d7a739d87cc43b0c43bd312d6806689
Alena Safyannikova <alena_safyanniko@mail.ru>
2019-12-12 10:13:16 +0500

    init repo

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <div id="app">
+            <h1>HELLO</h1>
+            <img alt="Vue logo" src="./assets/logo.png" />
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style></style>
